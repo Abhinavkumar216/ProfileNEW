c711d006e9eabcc45f4c7b77adf3d6507ca5c765
Abhinavkumar216 <krabhinav216@gmail.com>
2022-03-20 22:37:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {Profile} from './screens/index';
+function App() {
+  return (
+    <div className="App">
+      {/* <Home></Home> */}
+      <Profile></Profile>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar} from '../components/layouts/navbar/Navbar'
+// profile page
+export {default as Header} from '../components/profile/header/Header'
+export {default as Verification} from '../components/profile/verification/Verification'
+export {default as History} from '../components/profile/history/History'
+export {default as Info} from '../components/profile/info/Info'
